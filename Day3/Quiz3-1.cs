ca0872af89198b61643f5860cd3c52a5c79d57c9
sound2862 <62818790+sound2862@users.noreply.github.com>
2024-07-01 09:19:04 +0900

    Create Quiz3-1.cs

---
+++ Day3/Quiz3-1.cs
@@ -0,0 +1,26 @@
+namespace Quiz3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string str = Console.ReadLine().ToLower();
+
+                if (str == "y")
+                {
+                    continue;
+                }
+                else if (str == "n")
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+            }
+    }
+}
