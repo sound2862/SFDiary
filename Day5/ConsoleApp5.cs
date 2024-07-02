ceb0eccef9cbbbecde824b054783c9f68cebfc80
sound2862 <62818790+sound2862@users.noreply.github.com>
2024-07-02 09:02:14 +0900

    Create ConsoleApp5.cs

---
+++ Day5/ConsoleApp5.cs
@@ -0,0 +1,17 @@
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //while문으로 77~700까지 출력.
+
+            int a = 77;
+
+            while (a <= 700)
+            {
+                Console.WriteLine(a++); 
+            }
+        }
+    }
+}
